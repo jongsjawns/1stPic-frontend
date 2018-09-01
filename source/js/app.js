b5f154f775ea16b4e2dfe61602fc6535f93b1d89
jongsjawns <contactofjong@gmail.com>
2018-09-01 09:31:55 -0400

    Remove comments

--- source/js/app.js
+++ source/js/app.js
@@ -1,15 +1,10 @@
 var navButton = document.getElementById('nav-toggle-button');
 
 function toggleNavDisplay(event) {
-  // prevent hyperlink from running
   event.preventDefault();
 
-  // get the resonsive-nav container
   var responsiveNav = document.getElementById('responsive-nav');
 
-  // toggle the showNav CSS on the responsive nav container
-  // if the showNav class is there, it is removed; else it is added
-  // with a single property: toggle
   responsiveNav.classList.toggle('showNav');
 }
 
